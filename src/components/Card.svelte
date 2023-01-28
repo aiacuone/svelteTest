1575c57c397d50efc6642b87c73389b7c4fdebd8
aiacuone <age.iacuone@aventus.io>
2023-01-28 10:50:13 +0000

    first commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let person;
+	export let onFavourite;
+
+	const { isFavourite, uuid, name, image } = person;
+	let favouriteButtonColor = 'red';
+
+	const onFavouriteLocal = () => {
+		favouriteButtonColor = isFavourite ? 'green' : 'blue';
+		onFavourite({ id: uuid, boolean: !isFavourite });
+	};
+
+	const mapArray = { name, uuid, isFavourite };
+</script>
+
+<div class="border-2 border-black p-4">
+	<ul>
+		{#each Object.entries(mapArray) as [key, value]}
+			<li>{key}: {value}</li>
+		{/each}
+	</ul>
+	<img src={image} alt="test" />
+
+	<button on:click={onFavouriteLocal} class={`w-full py-2 bg-${favouriteButtonColor}-500`}
+		>Favourite</button
+	>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./src/pages/**/*.{js,jsx,ts,tsx}',
+		'./src/sections/**/*.{js,jsx,ts,tsx}',
+		'./src/components/**/*.{js,jsx,ts,tsx}'
+	],
+	theme: {
+		// extend: {}
+	},
+	plugins: []
+};
