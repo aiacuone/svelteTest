a374e0433459d775a9ef4c1515ab05eedf0f9ac7
aiacuone <age.iacuone@aventus.io>
2023-01-28 13:56:31 +0000

    let isFavourite isnt of directly changing export

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,24 +1,23 @@
 <script>
 	export let person;
 	export let onUpdatePerson;
-
+	let { isFavourite } = person;
 	console.log('person', ' L29 @ card.svelte');
 
 	const onFavourite = () => {
 		onUpdatePerson({
 			uuid: person.uuid,
 			key: 'isFavourite',
-			value: !person.isFavourite
+			value: !isFavourite
 		});
 	};
 </script>
 
 <div class="flex flex-col gap-3 border-2 border-black p-3">
 	<h1>{person.name}</h1>
-	<p>{person.isFavourite}</p>
-	<button
-		class={`bg-gray-${person.isFavourite ? '300' : '200'} p-2 rounded-md`}
-		on:click={onFavourite}>favourite</button
+	<p>{isFavourite}</p>
+	<button class={`bg-gray-${isFavourite ? '300' : '200'} p-2 rounded-md`} on:click={onFavourite}
+		>favourite</button
 	>
 	<img src={person.image} alt="test" />
 </div>
